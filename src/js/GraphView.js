7535ef96d78c3a656269a89cabea9cb7723281fe
Ivan Protsyuk <iprotsyuk@gmail.com>
2016-05-06 12:07:19 +0200

    Updated intensity axis name

--- src/js/GraphView.js
+++ src/js/GraphView.js
@@ -481,7 +481,7 @@ function createXicChart(div_id, dataProvider, graphs, guides) {
             'id': 'y',
             'dashLength': 1,
             'position': 'left',
-            'title': 'Intensity [number of ions]',
+            'title': 'Ion count',
             'labelFunction': formatNumberAsExponential
         }],
         'chartScrollbar': generatePlotScrollbarDescriptor(),
@@ -575,7 +575,7 @@ function createMassPeakChart(div_id, dataProvider, graphs, fragmentationSpectra)
             'id': 'y',
             'dashLength': 1,
             'position': 'left',
-            'title': 'Intensity [number of ions]',
+            'title': 'Ion count',
             'labelFunction': formatNumberAsExponential
         }],
         'export': generatePlotExportDescriptor(graphExporter.massPeakChartId),
